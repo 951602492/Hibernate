01345a9c5f8600fedd3df8de92d91bb38a5db9d0
CKW <232>
2017-01-14 10:24:47 +0800

    222

---
+++ Mokke/cn/hahahh/hahhah/mmTest.java
@@ -0,0 +1,10 @@
+package cn.hahahh.hahhah;
+
+/**
+ * Created by CKW on 2017/1/14.
+ */
+public class mmTest {
+    public static void  main(String[] args){
+        System.out.print("ahhahha");
+    }
+}
